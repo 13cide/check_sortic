02834fbfb58c136bb4d6d316cf672ea32221e39d
13cide <v.v.sidorovgg@gmail.com>
2022-02-01 21:13:53 +0300

    123

--- check_sort.h
+++ check_sort.h
@@ -4,11 +4,18 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <windows.h>
 using namespace std;
 
 int string_to_int(string str);
 int pow(int n, int i);
 int itc_len(string str);
+bool is_sorted(vector<int> &v);
+
+void start(vector<int> &a, vector<int> &b, vector<string> commands);
+vector<string> commands_to_mass();
+void initialize_a(vector<int> &a);
 
 void p(vector <int> &v_to, vector <int> &v_from);
 void r(vector <int> &v);
--- dop_funcs.cpp
+++ dop_funcs.cpp
@@ -20,3 +20,10 @@ int pow(int n, int i) {
     if (i == 0) return 1;
     else return n * pow(n, i - 1);
 }
+
+bool is_sorted(vector<int> &v) { // проверка на отсортированность
+    if (v.size() > 2) {
+        for (int i = 0; i < v.size() - 2; i++) if (v[i + 1] < v[i]) return false;
+    }
+    return true;
+}
